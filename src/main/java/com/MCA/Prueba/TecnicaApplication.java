05d922bec87ea14515f79537300f20f0305c0cfb
ruano23 <carlosruano23@gmail.com>
2023-05-03 17:52:12 +0200

    primer commit

---
+++ src/main/java/com/MCA/Prueba/TecnicaApplication.java
@@ -0,0 +1,12 @@
+package com.MCA.Prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+@SpringBootApplication
+public class TecnicaApplication {
+
+	}
---
+++ src/test/java/com/MCA/Prueba/TecnicaApplicationTests.java
@@ -0,0 +1,53 @@
+package com.MCA.Prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+@SpringBootTest
+class TecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	public static final String SEPARADOR = ",";
+
+	public static void main(String[] args) {
+
+		BufferedReader bufferLectura = null;
+		try {
+			// Abrir el .csv en buffer de lectura
+			bufferLectura = new BufferedReader(new FileReader("product.csv"));
+
+			// Leer una linea del archivo
+			String linea = bufferLectura.readLine();
+
+			while (linea != null) {
+				// Sepapar la linea leída con el separador definido previamente
+				String[] campos = linea.split(SEPARADOR);
+
+				System.out.println(Arrays.toString(campos));
+
+				// Volver a leer otra línea del fichero
+				linea = bufferLectura.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// Cierro el buffer de lectura
+			if (bufferLectura != null) {
+				try {
+					bufferLectura.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+}
